381c681a1d9b3674608a965f603aa06835ad5c18
Mohamad Abdulkarim <engineerdia@gmail.com>
2019-02-14 22:32:11 -0500

    added urls for python exercises

--- test.py
+++ test.py
@@ -21,5 +21,10 @@ class py_solution:
             i += 1
         return roman_num
 
+    urls = ['https://www.google.com/search?q=python+class+challenges+and+solutions&rlz=1C1SQJL_enUS818US818&oq=python'
+            '+class+challenges+and+solutions&aqs=chrome..69i57.10308j0j7&sourceid=chrome&ie=UTF-8',
+            'https://www.w3resource.com/python-exercises/class-exercises/', 'https://www.practicepython.org/', 'https'
+            '://www.techbeamers.com/python-tutorial-step-by-step/']
+
 
 print(py_solution().int_to_Roman(1))
