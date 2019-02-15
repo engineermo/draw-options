d9190c01ace58a8e0c22afefe3c4ac08e360ca41
Mohamad Abdulkarim <engineerdia@gmail.com>
2019-02-14 22:27:38 -0500

    added int to roman  converter

--- Lot.py
+++ Lot.py
@@ -9,13 +9,16 @@ def lot():
     user_input = int(user_input)
 
     for i in range(user_input):
+        print("\n")
         user_options = input("Please enter the " + str(i + 1) + " option\n" + str(i + 1) + "." + "  ")
         options.append(user_options)
         with open('data.txt', 'w+') as nf:
             nf.write('\n'.join(options))
     print("\n")
-    sleep(3)
+
     print(random.choice(options))
+
+    sleep(300)
 
 
 lot()
